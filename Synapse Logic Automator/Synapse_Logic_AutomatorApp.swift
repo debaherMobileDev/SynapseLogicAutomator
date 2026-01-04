89270613c8dad1326eb1a3de4daac1ea70cabc9e
ludik <bakhanetssimon@gmail.com>
2026-01-04 13:47:01 +0700

    Initial Commit

---
+++ Synapse Logic Automator/Synapse_Logic_AutomatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Synapse_Logic_AutomatorApp.swift
+//  Synapse Logic Automator
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Synapse_Logic_AutomatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Synapse Logic AutomatorTests/Synapse_Logic_AutomatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Synapse_Logic_AutomatorTests.swift
+//  Synapse Logic AutomatorTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct Synapse_Logic_AutomatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
